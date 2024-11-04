4d4f47aa7a929c9f8f68921f9b8d3488001c896e
espdesign <evanpendergraft@gmail.com>
2024-11-03 19:15:17 -0500

    reorganize main file

--- actclivity/actclivity.py
+++ actclivity/actclivity.py
@@ -5,17 +5,21 @@ import argparse
 import events
 import request
 
+
+def main(data):
+    try:
+        activity = events.Activity(data)
+        for event in activity.events:
+            print(event)
+
+    except TypeError:
+        print("Error: Username returned no data, please provide a valid active user")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Echo your input")
     parser.add_argument("username", help="Username to fetch github user activity.")
 
-args = parser.parse_args()
-
-data = request.github(args.username)
-try:
-    activity = events.Activity(data)
-    for event in activity.events:
-        print(event)
-
-except TypeError:
-    print("Error: Username returned no data, please provide a valid active user")
+    args = parser.parse_args()
+    data = request.github(args.username)
+    main(data)
